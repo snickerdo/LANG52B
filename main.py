f46b406e0441926b3e6d15728eff9dc9268b9a4d
snickerdo <143821167+snickerdo@users.noreply.github.com>
2023-09-07 14:02:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+num3 = float(input("Enter the third number:"))
+num4 = float(input("Enter the fourth number:"))
+
+sum = num1 + num2 + num3 + num4
+avg = sum / 4
+
+print(f"The sum is {sum}")
+print(f"The avg is {avg}")
